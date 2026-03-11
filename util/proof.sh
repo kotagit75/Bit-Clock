450e978854291156ba42e2c808c84c3559ebd265
kotagit75 <unity75lets5@gmail.com>
2026-03-11 17:14:41 +0900

    delete util

--- util/proof.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sendData='{"data":"${1}"}'
-echo $sendData
-# curl -X POST -H "Content-Type: application/json" -d '{"data":"${1}"}' localhost:8080/proof
-# curl -X POST -H "Content-Type: application/json" -d '{"data":"Hello world!"}' localhost:8080/proof
