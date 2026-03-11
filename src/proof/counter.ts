6160ac6985106301002470b780de0bdc8781dfaf
kotagit75 <unity75lets5@gmail.com>
2026-03-11 17:03:54 +0900

    organize

--- src/proof/counter.ts
+++ src/proof/counter.ts
@@ -1,6 +1,5 @@
-class Counter {
+export class Counter {
     constructor(private count: number){}
-
     increment(){
         return this.count++
     }
@@ -8,22 +7,3 @@ class Counter {
         return this.count
     }
 }
-
-function counterTest1(): boolean {
-    return new Counter(0).getCount() == 0
-}
-function counterTest2(): boolean {
-    return new Counter(0).increment() == 0
-}
-function counterTest3(): boolean {
-    var counter = new Counter(0)
-    counter.increment()
-    return counter.getCount() == 1
-}
-function counterTest() {
-    console.log("counterTest1:", counterTest1())
-    console.log("counterTest2:", counterTest2())
-    console.log("counterTest3:", counterTest3())
-}
-
-export { Counter }
